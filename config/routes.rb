9b6ec7affcd8286ec23349b6b8c9a832bdcefe4c
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2019-10-31 16:37:58 -0500

    Chapter4Completed

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: "dashboard#index"
+    get '/users/sign_out',:to => 'devise/sessions#destroy'
+    get '/users/sign_in', :to => 'devise/sessions#new'
+    resources :customers, only: [:index]
+end
